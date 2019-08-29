81dd18001339bfa51fa9ce5a7fb43b2921735212
Acer <pamoshakishani@gmail.com>
2019-08-29 11:52:08 +0530

    attendance interface

--- Employee Management/Attendance.Designer.cs
+++ Employee Management/Attendance.Designer.cs
@@ -28,9 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TabPage tabView;
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabAttendance = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.comboMonth = new System.Windows.Forms.ComboBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.tabInsert = new System.Windows.Forms.TabPage();
+            this.txtLeavingTime = new System.Windows.Forms.TextBox();
+            this.txtArrivingTime = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtEmployeeId = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblLeavingTme = new System.Windows.Forms.Label();
+            this.lblArrivingTime = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblEmployeeId = new System.Windows.Forms.Label();
+            this.ssss = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.EmpId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ArrivedTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LeftTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            tabView = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabAttendance.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabInsert.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ssss)).BeginInit();
+            tabView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -55,16 +92,326 @@
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabAttendance);
+            this.tabControl1.Controls.Add(this.tabInsert);
+            this.tabControl1.Controls.Add(tabView);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(3, 162);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(680, 343);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabAttendance
+            // 
+            this.tabAttendance.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabAttendance.Controls.Add(this.pictureBox1);
+            this.tabAttendance.Controls.Add(this.panel1);
+            this.tabAttendance.Location = new System.Drawing.Point(4, 27);
+            this.tabAttendance.Name = "tabAttendance";
+            this.tabAttendance.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAttendance.Size = new System.Drawing.Size(672, 312);
+            this.tabAttendance.TabIndex = 0;
+            this.tabAttendance.Text = "Attendance";
+            this.tabAttendance.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnGenerate);
+            this.panel1.Controls.Add(this.lblYear);
+            this.panel1.Controls.Add(this.comboMonth);
+            this.panel1.Controls.Add(this.txtYear);
+            this.panel1.Controls.Add(this.lblMonth);
+            this.panel1.Location = new System.Drawing.Point(339, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 214);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(48, 141);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(209, 33);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate Report";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(45, 35);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(38, 18);
+            this.lblYear.TabIndex = 4;
+            this.lblYear.Text = "Year";
+            // 
+            // comboMonth
+            // 
+            this.comboMonth.AutoCompleteCustomSource.AddRange(new string[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"});
+            this.comboMonth.FormattingEnabled = true;
+            this.comboMonth.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "02",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"});
+            this.comboMonth.Location = new System.Drawing.Point(124, 83);
+            this.comboMonth.Name = "comboMonth";
+            this.comboMonth.Size = new System.Drawing.Size(133, 26);
+            this.comboMonth.TabIndex = 3;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(124, 35);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(133, 24);
+            this.txtYear.TabIndex = 2;
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Location = new System.Drawing.Point(45, 83);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(50, 18);
+            this.lblMonth.TabIndex = 1;
+            this.lblMonth.Text = "Month";
+            // 
+            // tabInsert
+            // 
+            this.tabInsert.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabInsert.Controls.Add(this.txtLeavingTime);
+            this.tabInsert.Controls.Add(this.txtArrivingTime);
+            this.tabInsert.Controls.Add(this.dateTimePicker1);
+            this.tabInsert.Controls.Add(this.txtEmployeeId);
+            this.tabInsert.Controls.Add(this.btnInsert);
+            this.tabInsert.Controls.Add(this.lblLeavingTme);
+            this.tabInsert.Controls.Add(this.lblArrivingTime);
+            this.tabInsert.Controls.Add(this.lblDate);
+            this.tabInsert.Controls.Add(this.lblEmployeeId);
+            this.tabInsert.Controls.Add(this.ssss);
+            this.tabInsert.Location = new System.Drawing.Point(4, 27);
+            this.tabInsert.Name = "tabInsert";
+            this.tabInsert.Padding = new System.Windows.Forms.Padding(3);
+            this.tabInsert.Size = new System.Drawing.Size(669, 312);
+            this.tabInsert.TabIndex = 1;
+            this.tabInsert.Text = "Insert Attendance";
+            this.tabInsert.UseVisualStyleBackColor = true;
+            // 
+            // txtLeavingTime
+            // 
+            this.txtLeavingTime.Location = new System.Drawing.Point(287, 176);
+            this.txtLeavingTime.Name = "txtLeavingTime";
+            this.txtLeavingTime.Size = new System.Drawing.Size(200, 24);
+            this.txtLeavingTime.TabIndex = 9;
+            // 
+            // txtArrivingTime
+            // 
+            this.txtArrivingTime.Location = new System.Drawing.Point(287, 138);
+            this.txtArrivingTime.Name = "txtArrivingTime";
+            this.txtArrivingTime.Size = new System.Drawing.Size(200, 24);
+            this.txtArrivingTime.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(287, 93);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 24);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // txtEmployeeId
+            // 
+            this.txtEmployeeId.Location = new System.Drawing.Point(287, 54);
+            this.txtEmployeeId.Name = "txtEmployeeId";
+            this.txtEmployeeId.Size = new System.Drawing.Size(200, 24);
+            this.txtEmployeeId.TabIndex = 6;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(152, 226);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(335, 26);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // lblLeavingTme
+            // 
+            this.lblLeavingTme.AutoSize = true;
+            this.lblLeavingTme.BackColor = System.Drawing.Color.White;
+            this.lblLeavingTme.Location = new System.Drawing.Point(149, 176);
+            this.lblLeavingTme.Name = "lblLeavingTme";
+            this.lblLeavingTme.Size = new System.Drawing.Size(99, 18);
+            this.lblLeavingTme.TabIndex = 4;
+            this.lblLeavingTme.Text = "Leaving Time:";
+            // 
+            // lblArrivingTime
+            // 
+            this.lblArrivingTime.AutoSize = true;
+            this.lblArrivingTime.BackColor = System.Drawing.Color.White;
+            this.lblArrivingTime.Location = new System.Drawing.Point(149, 138);
+            this.lblArrivingTime.Name = "lblArrivingTime";
+            this.lblArrivingTime.Size = new System.Drawing.Size(97, 18);
+            this.lblArrivingTime.TabIndex = 3;
+            this.lblArrivingTime.Text = "Arriving Time:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(149, 93);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(43, 18);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date:";
+            // 
+            // lblEmployeeId
+            // 
+            this.lblEmployeeId.AutoSize = true;
+            this.lblEmployeeId.BackColor = System.Drawing.Color.White;
+            this.lblEmployeeId.Location = new System.Drawing.Point(149, 57);
+            this.lblEmployeeId.Name = "lblEmployeeId";
+            this.lblEmployeeId.Size = new System.Drawing.Size(93, 18);
+            this.lblEmployeeId.TabIndex = 1;
+            this.lblEmployeeId.Text = "Employee Id:";
+            // 
+            // ssss
+            // 
+            this.ssss.BackColor = System.Drawing.Color.Transparent;
+            this.ssss.Location = new System.Drawing.Point(93, 28);
+            this.ssss.Name = "ssss";
+            this.ssss.Size = new System.Drawing.Size(474, 260);
+            this.ssss.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ssss.TabIndex = 0;
+            this.ssss.TabStop = false;
+            // 
+            // tabView
+            // 
+            tabView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            tabView.Controls.Add(this.dataGridView1);
+            tabView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            tabView.Location = new System.Drawing.Point(4, 27);
+            tabView.Name = "tabView";
+            tabView.Size = new System.Drawing.Size(669, 312);
+            tabView.TabIndex = 2;
+            tabView.Text = "View Attendance";
+            tabView.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmpId,
+            this.Date,
+            this.ArrivedTime,
+            this.LeftTime,
+            this.Edit,
+            this.Delete});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 215);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // EmpId
+            // 
+            this.EmpId.DataPropertyName = "EmpID";
+            this.EmpId.HeaderText = "EmpId";
+            this.EmpId.Name = "EmpId";
+            this.EmpId.ReadOnly = true;
+            this.EmpId.Width = 75;
+            // 
+            // Date
+            // 
+            this.Date.DataPropertyName = "date";
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // ArrivedTime
+            // 
+            this.ArrivedTime.DataPropertyName = "inTime";
+            this.ArrivedTime.HeaderText = "Arrived Time";
+            this.ArrivedTime.Name = "ArrivedTime";
+            this.ArrivedTime.ReadOnly = true;
+            this.ArrivedTime.Width = 125;
+            // 
+            // LeftTime
+            // 
+            this.LeftTime.DataPropertyName = "outTime";
+            this.LeftTime.HeaderText = "Left Time";
+            this.LeftTime.Name = "LeftTime";
+            this.LeftTime.ReadOnly = true;
+            // 
+            // Edit
+            // 
+            this.Edit.HeaderText = "EDIT";
+            this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            this.Edit.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Edit.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "DELETE";
+            this.Delete.Name = "Delete";
+            this.Delete.ReadOnly = true;
+            this.Delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Delete.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // Attendance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label1);
             this.Name = "Attendance";
             this.Size = new System.Drawing.Size(683, 508);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabAttendance.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabInsert.ResumeLayout(false);
+            this.tabInsert.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ssss)).EndInit();
+            tabView.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,5 +421,32 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabAttendance;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.ComboBox comboMonth;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.TabPage tabInsert;
+        private System.Windows.Forms.TextBox txtLeavingTime;
+        private System.Windows.Forms.TextBox txtArrivingTime;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtEmployeeId;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblLeavingTme;
+        private System.Windows.Forms.Label lblArrivingTime;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblEmployeeId;
+        private System.Windows.Forms.PictureBox ssss;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmpId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ArrivedTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LeftTime;
+        private System.Windows.Forms.DataGridViewButtonColumn Edit;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
     }
 }
